48a6ea53ba3fb7abd2efd3443325f223e21f6320
Vinayak.S <indrevler105@gmail.com>
2025-06-15 09:51:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,201 +1,193 @@
-$(document).ready(function() {
-  var anim_id;
+document.addEventListener("DOMContentLoaded", function () {
+  let anim_id;
 
-  var container = $('#container');
-  var car = $('#car');
-  var car_1 = $('#car_1');
-  var car_2 = $('#car_2');
-  var car_3 = $('#car_3');
-  var line_1 = $('#line_1');
-  var line_2 = $('#line_2');
-  var line_3 = $('#line_3');
-  var restart_div = $('#restart_div');
-  var restart_btn = $('#restart');
-  var score = $('#score');
+  const container = document.getElementById("container");
+  const car = document.getElementById("car");
+  const car_1 = document.getElementById("car_1");
+  const car_2 = document.getElementById("car_2");
+  const car_3 = document.getElementById("car_3");
+  const line_1 = document.getElementById("line_1");
+  const line_2 = document.getElementById("line_2");
+  const line_3 = document.getElementById("line_3");
+  const restart_div = document.getElementById("restart_div");
+  const restart_btn = document.getElementById("restart");
+  const score = document.getElementById("score");
 
-  var container_left = parseInt(container.css('left'));
-  var container_width = parseInt(container.width());
-  var container_height = parseInt(container.height());
-  var car_width = parseInt(car.width());
-  var car_height = parseInt(car.height());
+  const container_width = container.offsetWidth;
+  const container_height = container.offsetHeight;
+  const car_width = car.offsetWidth;
+  const car_height = car.offsetHeight;
 
-  var game_over = false;
+  let game_over = false;
+  let score_counter = 1;
+  let speed = 2;
+  let line_speed = 5;
 
-  var score_counter = 1;
+  let move_right = false;
+  let move_left = false;
+  let move_up = false;
+  let move_down = false;
 
-  var speed = 2;
-  var line_speed = 5;
-
-  var move_right = false;
-  var move_left = false;
-  var move_up = false;
-  var move_down = false;
-
-  $(document).on('keydown', function(e) {
-      if (game_over === false) {
-          var key = e.keyCode;
-          if (key === 37 && move_left === false) {
-              move_left = requestAnimationFrame(left);
-              sfx.move_left.play();
-          } else if (key === 39 && move_right === false) {
-              move_right = requestAnimationFrame(right);
-          } else if (key === 38 && move_up === false) {
-              move_up = requestAnimationFrame(up);
-          } else if (key === 40 && move_down === false) {
-              move_down = requestAnimationFrame(down);
-          }
+  document.addEventListener("keydown", function (e) {
+    if (!game_over) {
+      const key = e.keyCode;
+      if (key === 37 && !move_left) {
+        move_left = requestAnimationFrame(left);
+        if (window.sfx?.move_left) window.sfx.move_left.play();
+      } else if (key === 39 && !move_right) {
+        move_right = requestAnimationFrame(right);
+      } else if (key === 38 && !move_up) {
+        move_up = requestAnimationFrame(up);
+      } else if (key === 40 && !move_down) {
+        move_down = requestAnimationFrame(down);
       }
+    }
   });
 
-  $(document).on('keyup', function(e) {
-      if (game_over === false) {
-          var key = e.keyCode;
-          if (key === 37) {
-              cancelAnimationFrame(move_left);
-              move_left = false;
-          } else if (key === 39) {
-              cancelAnimationFrame(move_right);
-              move_right = false;
-          } else if (key === 38) {
-              cancelAnimationFrame(move_up);
-              move_up = false;
-          } else if (key === 40) {
-              cancelAnimationFrame(move_down);
-              move_down = false;
-          }
+  document.addEventListener("keyup", function (e) {
+    if (!game_over) {
+      const key = e.keyCode;
+      if (key === 37) {
+        cancelAnimationFrame(move_left);
+        move_left = false;
+      } else if (key === 39) {
+        cancelAnimationFrame(move_right);
+        move_right = false;
+      } else if (key === 38) {
+        cancelAnimationFrame(move_up);
+        move_up = false;
+      } else if (key === 40) {
+        cancelAnimationFrame(move_down);
+        move_down = false;
       }
+    }
   });
 
   function left() {
-      if (game_over === false && parseInt(car.css('left')) > 0) {
-          car.css('left', parseInt(car.css('left')) - 5);
-          move_left = requestAnimationFrame(left);
-      }
+    const currentLeft = parseInt(getComputedStyle(car).left);
+    if (!game_over && currentLeft > 0) {
+      car.style.left = currentLeft - 5 + "px";
+      move_left = requestAnimationFrame(left);
+    }
   }
 
   function right() {
-      if (game_over === false && parseInt(car.css('left')) < container_width - car_width) {
-          car.css('left', parseInt(car.css('left')) + 5);
-          move_right = requestAnimationFrame(right);
-      }
+    const currentLeft = parseInt(getComputedStyle(car).left);
+    if (!game_over && currentLeft < container_width - car_width) {
+      car.style.left = currentLeft + 5 + "px";
+      move_right = requestAnimationFrame(right);
+    }
   }
 
   function up() {
-      if (game_over === false && parseInt(car.css('top')) > 0) {
-          car.css('top', parseInt(car.css('top')) - 3);
-          move_up = requestAnimationFrame(up);
-      }
+    const currentTop = parseInt(getComputedStyle(car).top);
+    if (!game_over && currentTop > 0) {
+      car.style.top = currentTop - 3 + "px";
+      move_up = requestAnimationFrame(up);
+    }
   }
 
   function down() {
-      if (game_over === false && parseInt(car.css('top')) < container_height - car_height) {
-          car.css('top', parseInt(car.css('top')) + 3);
-          move_down = requestAnimationFrame(down);
-      }
+    const currentTop = parseInt(getComputedStyle(car).top);
+    if (!game_over && currentTop < container_height - car_height) {
+      car.style.top = currentTop + 3 + "px";
+      move_down = requestAnimationFrame(down);
+    }
   }
 
   anim_id = requestAnimationFrame(repeat);
 
   function repeat() {
-      if (collision(car, car_1) || collision(car, car_2) || collision(car, car_3)) {
-          stop_the_game();
-          return;
-      }
+    if (collision(car, car_1) || collision(car, car_2) || collision(car, car_3)) {
+      stop_the_game();
+      return;
+    }
 
-      score_counter++;
+    score_counter++;
 
-      if (score_counter % 20 == 0) {
-          score.text(parseInt(score.text()) + 1);
-      }
-      if (score_counter % 500 == 0) {
-          speed++;
-          line_speed++;
-      }
+    if (score_counter % 20 === 0) {
+      score.innerText = parseInt(score.innerText) + 1;
+    }
 
-      car_down(car_1);
-      car_down(car_2);
-      car_down(car_3);
+    if (score_counter % 500 === 0) {
+      speed++;
+      line_speed++;
+    }
 
-      line_down(line_1);
-      line_down(line_2);
-      line_down(line_3);
+    car_down(car_1);
+    car_down(car_2);
+    car_down(car_3);
+    line_down(line_1);
+    line_down(line_2);
+    line_down(line_3);
 
-      anim_id = requestAnimationFrame(repeat);
+    anim_id = requestAnimationFrame(repeat);
   }
 
-  function car_down(car) {
-      var car_current_top = parseInt(car.css('top'));
-      if (car_current_top > container_height) {
-          car_current_top = -200;
-          var car_left = parseInt(Math.random() * (container_width - car_width));
-          car.css('left', car_left);
-      }
-      car.css('top', car_current_top + speed);
+  function car_down(car_obj) {
+    let top = parseInt(getComputedStyle(car_obj).top);
+    if (top > container_height) {
+      top = -200;
+      const left = Math.floor(Math.random() * (container_width - car_width));
+      car_obj.style.left = left + "px";
+    }
+    car_obj.style.top = top + speed + "px";
   }
 
-  function line_down(line) {
-      var line_current_top = parseInt(line.css('top'));
-      if (line_current_top > container_height) {
-          line_current_top = -300;
-      }
-      line.css('top', line_current_top + line_speed);
+  function line_down(line_obj) {
+    let top = parseInt(getComputedStyle(line_obj).top);
+    if (top > container_height) {
+      top = -300;
+    }
+    line_obj.style.top = top + line_speed + "px";
   }
 
-  restart_btn.click(function() {
-      location.reload();
+  restart_btn.addEventListener("click", () => {
+    location.reload();
   });
 
   function stop_the_game() {
-      game_over = true;
-      cancelAnimationFrame(anim_id);
-      cancelAnimationFrame(move_right);
-      cancelAnimationFrame(move_left);
-      cancelAnimationFrame(move_up);
-      cancelAnimationFrame(move_down);
-      restart_div.slideDown();
-      restart_btn.focus();
-      const song = document.getElementById("song");
-      song.pause();    
-      song.currentTime = 0; 
-  }
-  
-  $(document).on('click', function() {
-      if (game_over === true) {
-          location.reload();
-      }
-  });
+    game_over = true;
+    cancelAnimationFrame(anim_id);
+    cancelAnimationFrame(move_right);
+    cancelAnimationFrame(move_left);
+    cancelAnimationFrame(move_up);
+    cancelAnimationFrame(move_down);
 
+    restart_div.style.display = "block";
+    restart_btn.focus();
 
-  function collision($div1, $div2) {
-      var x1 = $div1.offset().left;
-      var y1 = $div1.offset().top;
-      var h1 = $div1.outerHeight(true);
-      var w1 = $div1.outerWidth(true);
-      var b1 = y1 + h1;
-      var r1 = x1 + w1;
-      var x2 = $div2.offset().left;
-      var y2 = $div2.offset().top;
-      var h2 = $div2.outerHeight(true);
-      var w2 = $div2.outerWidth(true);
-      var b2 = y2 + h2;
-      var r2 = x2 + w2;
-
-      if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) return false;
-      return true;
-  }
- let isReady = false;
-
-  window.addEventListener('click', () => {
     const song = document.getElementById("song");
-    song.play();     
-    song.pause();  
-    isReady = true;     
+    song.pause();
+    song.currentTime = 0;
+  }
+
+  document.addEventListener("click", () => {
+    if (game_over) location.reload();
   });
 
-window.addEventListener('click', () => {
-  const song = document.getElementById("song");
-  song.play();
-});
+  function collision(el1, el2) {
+    const rect1 = el1.getBoundingClientRect();
+    const rect2 = el2.getBoundingClientRect();
 
+    return !(
+      rect1.top + rect1.height < rect2.top ||
+      rect1.top > rect2.top + rect2.height ||
+      rect1.left + rect1.width < rect2.left ||
+      rect1.left > rect2.left + rect2.width
+    );
+  }
+  let isReady = false;
 
+  window.addEventListener("click", () => {
+    const song = document.getElementById("song");
+    if (!isReady) {
+      song.play().then(() => {
+        song.pause();
+        isReady = true;
+      }).catch(() => {});
+    } else {
+      song.play();
+    }
+  });
 });
